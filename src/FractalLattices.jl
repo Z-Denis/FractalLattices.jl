794812e3537a09041cf816ba23631f1aced7d90e
Zakari Denis <zakari.denis@gmail.com>
2022-03-25 15:15:45 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/FractalLattices.jl
@@ -0,0 +1,5 @@
+module FractalLattices
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FractalLattices
+using Test
+
+@testset "FractalLattices.jl" begin
+    # Write your tests here.
+end
